deac08c388fbe40d14303b15e7a4510b362372d3
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-07-18 14:19:48 +0545

    Solution added.

---
+++ TestSolution/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestSolution
+{
+    class Program
+    {
+        static String location;
+        static DateTime time;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(location == null ? "Location is null." : location);
+            Console.WriteLine(time == null ? "Time is null." : time.ToString());
+            Console.ReadKey();
+        }
+    }
+}
